f56fe3cbceb8a3ab987f8b5827f083b6d05f178f
AKavaa <alex.kavaleuskiy33@gmail.com>
2025-11-04 23:35:37 +0200

    Loading screen input and basic functionalities

---
+++ main/main.js
@@ -0,0 +1,18 @@
+function startGame() {
+    const username = document.getElementById('username').value.trim();
+
+
+    // verify that the user inputs a valdi username and not blank 
+    if (username === "") {
+        alert("Please enter a valid Username to start!");
+        return 0;
+    }
+
+    // save username in the local storage
+    localStorage.setItem("username", username);
+
+    window.location.href = "../src/entry_level/entry.html";
+}
+
+
+
