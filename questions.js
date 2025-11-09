a22298993688e22882e610fde6876abbb824cfda
AKavaa <alex.kavaleuskiy33@gmail.com>
2025-11-09 17:16:03 +0200

    Score fix

--- questions.js
+++ questions.js
@@ -83,7 +83,7 @@ function checkAnswer(selectedIndex) {
     optionButtons.forEach(button => button.disabled = true);
 
     if (selectedIndex === question.answer) {
-        score + 5;
+        score++;
         optionButtons[selectedIndex].classList.add('correct');
     } else {
         optionButtons[selectedIndex].classList.add('wrong');
